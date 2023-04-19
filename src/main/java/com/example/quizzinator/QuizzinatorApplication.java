fa799cabf42d00dcceeb08b4fd29951a52bffd09
taneli1 <leevi.limnell@fi.experis.com>
2023-04-19 14:30:10 +0300

    Initial commit.

---
+++ src/main/java/com/example/quizzinator/QuizzinatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.quizzinator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizzinatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizzinatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/quizzinator/QuizzinatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quizzinator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizzinatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
